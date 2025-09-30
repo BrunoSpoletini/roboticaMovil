7c1458e7b5eb60edd2a9684efbc98b1801c51e92
BrunoSpoletini <bruno_spoletini@hotmail.com>
2025-09-30 20:04:46 -0300

    cambio en los graficos

--- TP2/script.py
+++ TP2/script.py
@@ -5,12 +5,11 @@ import transforms3d.quaternions as quat
 import matplotlib.pyplot as plt
 from transforms3d import affines, euler, quaternions
 import csv
-import re 
-
-inputFile = "log.txt"
+import re
+import os 
 
 def parseInput(input_file):
-    output_file = "logParsed.csv"
+    output_file = f"{input_file}_parsed.csv"
 
     # regex para capturar sec y nanosec
     time_pattern = re.compile(r"sec=(\d+), nanosec=(\d+)")
@@ -19,7 +18,7 @@ def parseInput(input_file):
 
     started = False
 
-    with open(input_file, "r") as infile:
+    with open(f"{input_file}.txt", "r") as infile:
         for line in infile:
 
             time_match = time_pattern.search(line)
@@ -52,22 +51,60 @@ def parseInput(input_file):
             writer.writerow(header)
             writer.writerows(rows)
 
-parseInput(inputFile)
+def readPoses(input_file):
+    poses = []
+    with open(f"./{input_file}_parsed.csv", newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            if row[0].startswith("timestamp"):
+                continue
+            poses.append([float(x) for x in row[:8]])
+    return poses
 
-poses = []
-with open("./logParsed.csv", newline="") as csvfile:
-    reader = csv.reader(csvfile, delimiter=",")
-    for row in reader:
-        if row[0].startswith("timestamp"):
-            continue
-        poses.append([float(x) for x in row[:8]])
+def obtenerMaxMin(xs, ys, qs, vs, ws):
+    max_x = max(xs)
+    min_x = min(xs)
+    print("Rango X: ", min_x, " a ", max_x)
+    max_y = max(ys)
+    min_y = min(ys)
+    print("Rango Y: ", min_y, " a ", max_y)
+    max_q = max(qs)
+    min_q = min(qs)
+    print("Rango θ: ", min_q, " a ", max_q)
+    max_v = max(vs)
+    min_v = min(vs)
+    print("Rango Vel. linear: ", min_v, " a ", max_v)
+    max_w = max(ws)
+    min_w = min(ws)
+    print("Rango Vel. angular: ", min_w, " a ", max_w)
 
-def generarGrafico2D(x, y, x_label, y_label, tittle, pathfile, square=False, rad=False):
+    return (max_x, min_x, max_y, min_y, max_q, min_q, max_v, min_v, max_w, min_w)
+
+def generarGrafico2D(x, y, x_label, y_label, tittle, pathfile, square=False, rad=False, dots=False, orientations=None):
+
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111)
     ax.plot(
-        x, y, color="blue"
+        x, y, color="blue", label=tittle
     )
+    if(dots):
+        n = int(len(x) / 4)
+        indices = [n, 2*n, 3*n]
+        ax.scatter([x[i] for i in indices], [y[i] for i in indices],s=100, color="red", alpha=1)
+        ax.scatter([x[0]], [y[0]], s=100, marker="s", color="green", label="Inicio", alpha=1)
+        ax.scatter([x[-1]], [y[-1]], s=100, marker="^", color="green", label="Fin", alpha=1)
+        
+        if orientations is not None:
+            arrow_indices = indices + [0, len(x)-1]
+            for i in arrow_indices:
+                if i < len(orientations):
+                    dx = 0.3 * np.cos(orientations[i])
+                    dy = 0.3 * np.sin(orientations[i])
+                    ax.arrow(x[i], y[i], dx, dy, head_width=0.1, head_length=0.1, 
+                            fc='orange', ec='orange', alpha=1, width=0.01)
+
+
+
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
     ax.set_title(tittle)
@@ -82,7 +119,12 @@ def generarGrafico2D(x, y, x_label, y_label, tittle, pathfile, square=False, rad
     plt.tight_layout()
     plt.savefig(pathfile, dpi=300)
 
-def generarGraficos():
+def generarGraficos(inputFile, outputDir, dots=False):
+    os.makedirs(outputDir, exist_ok=True)
+    
+    parseInput(inputFile) # Genera csv parseado
+    poses = readPoses(inputFile) # Lee poses del csv
+
     ts = []
     xs = []
     ys = []  
@@ -95,27 +137,31 @@ def generarGraficos():
         ts.append(timestamp)
         xs.append(round(x, 4))
         ys.append(round(y, 4))
-        qs.append((round(q, 4) + 2*np.pi) % (2*np.pi))
+        qs.append(round(q, 4))
         vs.append(round(v, 4))
         ws.append(round(w, 4))
 
-
     # Mostrar Path
-    generarGrafico2D(xs, ys, "x", "y", "Camino", "path.png", square=True)
+    generarGrafico2D(xs, ys, "x", "y", "Camino", f"./{outputDir}/path.png", square=True, dots=dots, orientations=qs)
 
     # Mostrar Pose X
-    generarGrafico2D(ts, xs, "t", "x", "Pose X", "pose_x.png")
+    generarGrafico2D(ts, xs, "t", "x", "Trayectoria X", f"./{outputDir}/pose_x.png", dots=dots)
 
     # Mostrar Pose Y
-    generarGrafico2D(ts, ys, "t", "y", "Pose Y", "pose_y.png")
-    
+    generarGrafico2D(ts, ys, "t", "y", "Trayectoria Y", f"./{outputDir}/pose_y.png", dots=dots)
+
     # Mostrar Pose Orientacion
-    generarGrafico2D(ts, np.cos(qs), "t", "θ", "Pose θ", "pose_q.png", rad=True)
+    generarGrafico2D(ts, qs, "t", "θ", "Orientación θ", f"./{outputDir}/pose_q.png", rad=True, dots=dots)
 
     # Mostrar Velocidad Lineal
-    generarGrafico2D(ts, vs, "t", "v", "Velocidad Lineal", "vel_lin.png")
+    generarGrafico2D(ts, vs, "t", "v", "Velocidad Lineal", f"./{outputDir}/vel_lin.png")
 
     # Mostrar Velocidad Angular
-    generarGrafico2D(ts, ws, "t", "w", "Velocidad Angular", "vel_ang.png")
+    generarGrafico2D(ts, ws, "t", "w", "Velocidad Angular", f"./{outputDir}/vel_ang.png")
 
-generarGraficos()
+    # Mostrar valores maximos y minimos
+    obtenerMaxMin(xs, ys, qs, vs, ws)
+
+# generarGraficos("log", "ej5")
+
+generarGraficos("log", "ej6", dots=True)
