f3079830daeae24c66451263cd76bf8ad5090a51
BrunoSpoletini <bruno_spoletini@hotmail.com>
2025-10-06 21:00:51 +0000

    Fixes santiago

---
+++ TP2/e7d.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source /opt/ros/jazzy/setup.bash
+
+ros2 topic pub -1 --keep-alive 140 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"   ;
+ros2 topic pub -1 --keep-alive 63  /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.025}}" ;
+ros2 topic pub -1 --keep-alive 140 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"   ;
+ros2 topic pub -1 --keep-alive 63  /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.025}}" ;
+ros2 topic pub -1 --keep-alive 140 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"   ;
+ros2 topic pub -1 --keep-alive 63  /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.025}}" ;
+ros2 topic pub -1 --keep-alive 140 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"   ;
+ros2 topic pub -1 --keep-alive 63  /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.025}}" ;
+ros2 topic pub -1 --keep-alive 140 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"   ;
+ros2 topic pub -1 --keep-alive 63  /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.025}}" ;
+ros2 topic pub -1 --keep-alive 140 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"   ;
+ros2 topic pub -1 --keep-alive 63  /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.025}}" ;
+ros2 topic pub -1 --keep-alive 140 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"   ;
+ros2 topic pub -1 --keep-alive 63  /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.025}}" ;
+ros2 topic pub -1 --keep-alive 140 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"   ;
+ros2 topic pub -1 --keep-alive 63  /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.01, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.025}}"
