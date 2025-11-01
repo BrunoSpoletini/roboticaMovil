79d381e827b7244e44cfc1c72e6bab35fad3104c
Santiago Có <51292523+Santiagoco1@users.noreply.github.com>
2025-11-01 12:35:06 -0300

    Delete TP3/conversorRos2.py

--- TP3/conversorRos2.py
+++
@@ -1,6 +0,0 @@
-import os
-
-rosbags = ["cam_april", "imu_april", "cam_checkerboard"]
-
-for bag in rosbags:
-    os.system(f"rosbags-convert --src ./archivos/rosbags/ros1/{bag}.bag --dst ./archivos/rosbags/ros2/{bag}")
